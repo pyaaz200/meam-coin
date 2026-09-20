8273d37d69c0d89ecfd06e4f26b18a0748c9f848
Pyaaz <onion.byts@gmail.com>
2026-09-20 06:10:21 +0530

    fix: scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -10,9 +10,18 @@ import BuyCoin from './pages/BuyCoin';
 import HowToBuy from './pages/HowToBuy';
 import Terms from './pages/Terms';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="app-container">
         <Navbar />
         <main style={{ minHeight: 'calc(100vh - var(--nav-height) - 100px)' }}>
